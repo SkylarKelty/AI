eaee4349314e07a9af300f9235fce9ceaeabfde8
Skylar Kelty <skylarkelty@gmail.com>
2013-08-26 18:49:39 +0100

    Center window

--- src/simulator/sim.py
+++ src/simulator/sim.py
@@ -4,11 +4,19 @@ from PyQt4 import QtCore, QtGui
 class Window(QtGui.QMainWindow):
 	def __init__(self):
 		QtGui.QMainWindow.__init__(self)
-		self.setGeometry(300, 300, 300, 300)
+		self.setGeometry(0, 0, 800, 800)
 		self.setWindowTitle('AI Sim')
+
+	def center(self):
+		screen = QtGui.QDesktopWidget().screenGeometry()
+		size =  self.geometry()
+		x = (screen.width() - size.width()) / 2
+		y = (screen.height() - size.height()) / 2
+		self.move(x, y)
 
 def main():
 	app = QtGui.QApplication(sys.argv)
 	w = Window()
+	w.center()
 	w.show()
 	sys.exit(app.exec_())
