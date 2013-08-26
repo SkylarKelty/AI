528ff3d8380afa9d215263366b25481f229128e9
Skylar Kelty <skylarkelty@gmail.com>
2013-08-26 21:33:42 +0100

    Begin more work

--- src/ai/pathfinding.py
+++ src/ai/pathfinding.py
@@ -1,5 +1,5 @@
 #
-# Pathfinding class
+# Pathfinding class, avoids other objects
 #
 class Path(object):
 	# Create a new Path
@@ -18,10 +18,6 @@ class Path(object):
 	def setDestination(self, cell):
 		self.destination = cell
 
-	# Update the path
-	def update(self):
-		pass
-
 	# Returns the next node on the path
 	def next(self):
 		if self.path:
@@ -31,3 +27,26 @@ class Path(object):
 	# Returns the entire path
 	def path(self):
 		return self.path
+
+	# Update the path
+	def update(self):
+		self.path = self.getPath(self.source)
+
+	# Find a path from the supplied cell
+	def getPath(self, src):
+		best = self.getBest(src)
+
+	# Find the best node(s) we can get to from the current cell
+	def getBest(self, src):
+		x = src[0]
+		y = src[1]
+
+		for tX in range(x - 1, x + 1):
+			print tX
+
+			for tY in range(y - 1, y + 1):
+				print tY
+
+	# Return the weight of the given node
+	def weight(self, src, dest):
+		return abs((src[0] + src[1]) - (dest[0] + dest[1]))
